e467cf65d059b40f4111c70290e28e11a724d699
Wander Aquino <wander.aquino@gmail.com>
2021-10-06 01:42:25 -0300

    Import contextdata to use email data on dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,10 @@
-export function Dashboard () {
+import { useContext } from "react";
+import {AuthContext} from "../context/AuthContext";
+
+export default function Dashboard () {
+    const {user} = useContext(AuthContext);
+
     return (
-        <h1>Dashboard!!!</h1>
+        <h1>Dashboard!!! {user.email}</h1>
     )
 }
