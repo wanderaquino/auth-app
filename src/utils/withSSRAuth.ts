2e3124bf960f29232c4d9fe71c2f645e8829851b
Wander Aquino <wander.aquino@gmail.com>
2021-10-17 02:19:08 -0300

    Use usePermission validation function imported from userPermission file

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -1,15 +1,40 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { parseCookies } from "nookies";
+import decode from "jwt-decode";
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<P>(fn : GetServerSideProps<P>) {
+
+type WithSSRAuthOptions= {
+  permissions?: string[],
+  roles?: string[]
+}
+
+export function withSSRAuth<P>(fn : GetServerSideProps<P>, options? : WithSSRAuthOptions) {
     return async (context : GetServerSidePropsContext) : Promise<GetServerSidePropsResult<P>> => {
         const cookies = parseCookies(context);
+        const token = cookies["nextAuth.token"];
 
-        if (!cookies["nextAuth.token"]) {
+        if (!token) {
           return {
             redirect: {
               destination: "/",
               permanent: false
+            }
+          }
+        }
+        
+        if(options) {
+          const user = decode<{permissions: string[], roles: string []}>(token);
+          const {permissions, roles} = options as WithSSRAuthOptions;
+  
+          const userHasValidPermissions = validateUserPermissions({user, roles, permissions});
+
+          if(!userHasValidPermissions) {
+            return {
+              redirect: {
+                destination: "/dashboard",
+                permanent: false
+              }
             }
           }
         }
