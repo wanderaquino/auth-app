c60b0e5d4c55499649cfb78d1f5a48c18d1eb202
Wander Aquino <wander.aquino@gmail.com>
2021-10-18 00:15:58 -0300

    Redirect to dashboard page when no allowed to stay on metrics page

--- src/pages/metrics.tsx
+++ src/pages/metrics.tsx
@@ -1,10 +1,38 @@
 import { GetServerSidePropsContext } from "next";
+import { parseCookies } from "nookies";
+import { useEffect } from "react";
+import decode from "jwt-decode";
 import { setupApiClient } from "../services/api";
 import { withSSRAuth } from '../utils/withSSRAuth';
+import { validateUserPermissions } from "../utils/validateUserPermissions";
+import Router from "next/router";
 
 
 
 export default function Metrics () {
+    useEffect(() => {
+        const cookies = parseCookies();
+        const token = decode<{
+            user: {permissions: string[], roles: string[]}, 
+            roles: string[], 
+            permissions: string[]}>
+            (cookies["nextAuth.token"]);
+        
+        const userCanStayOnPage = validateUserPermissions({
+            user: {
+                permissions: token.permissions,
+                roles: token.roles
+            },
+            permissions: token.permissions,
+            roles: token.roles
+        });
+
+        if(!userCanStayOnPage) {
+            console.log("Redirect!!!!!!!!!!!");
+            Router.push("/dashboard");
+        }
+
+    }, []);
     return (
         <>
         <h1>Metrics</h1>
